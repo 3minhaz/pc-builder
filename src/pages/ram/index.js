b201832833deaa75fc80c83740a392c883cd55b2
3minhaz <3minhaz@gmail.com>
2023-07-30 16:07:31 +0600

    categories page update

--- src/pages/ram/index.js
+++ src/pages/ram/index.js
@@ -20,11 +20,11 @@ const RamPage = ({ ram }) => {
           </figure>
           <div className="card-body items-center text-center">
             <h2 className="card-title">{product?.productName}</h2>
-            <p>
+            {/* <p>
               {product?.description.length > 150
                 ? product?.description.slice(0, 150) + "...."
                 : product?.description}
-            </p>
+            </p> */}
             <div>
               <p>Price: {product?.price}</p>
               <p>Category: {product?.category}</p>
